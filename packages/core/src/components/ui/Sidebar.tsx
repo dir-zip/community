0e26a37b7ef0d4a4ffd38d8bd19d0d86ac2ad388
Dillon Raphael <dillon@creatorsneverdie.com>
2024-01-04 11:55:32 -0500

    break down sidebar into different component

--- packages/core/src/components/ui/Sidebar.tsx
+++ packages/core/src/components/ui/Sidebar.tsx
@@ -1,8 +1,9 @@
 "use client"
-import { Sidebar as SidebarContainer, type SidebarProps } from "@dir/ui"
+import { SidebarContainer, TagCloud, SiteInfo, NavWrapper, InnerSidebarContainer, type SidebarProps } from "@dir/ui"
 import { usePathname } from "next/navigation"
 import Link from 'next/link'
-import { Layers, Users, Store, FileText } from 'lucide-react'
+import { Layers, Settings, Users, LogOut, Store, FileText } from 'lucide-react'
+import { logoutAction } from "../../features/auth/actions"
 
 const _sidebarLinks = [
   { icon: Layers, text: "Feed", link: '/' },
@@ -14,13 +15,29 @@ const _sidebarLinks = [
 export const Sidebar = (props: Omit<SidebarProps, 'children'>) => {
   const pathname = usePathname()
   return (
-    <SidebarContainer siteTitle={props.siteTitle} memberCount={props.memberCount}>
-      {_sidebarLinks.map((link, i) => {
-        const Icon = link['icon']
-        return (
-          <Link href={link.link} key={i} className={`${pathname === link.link ? 'bg-primary-900' : null} text-sm rounded px-4 py-2 flex items-center space-x-2`}><Icon className="w-4 h-4" /><span>{link.text}</span></Link>
-        )
-      })}
+    <SidebarContainer>
+      <InnerSidebarContainer>
+        <SiteInfo siteTitle={props.siteTitle} memberCount={props.memberCount} />
+        <NavWrapper>
+          {_sidebarLinks.map((link, i) => {
+            const Icon = link['icon']
+            return (
+              <Link href={link.link} key={i} className={`${pathname === link.link ? 'bg-primary-900' : null} text-sm rounded px-4 py-2 flex items-center space-x-2`}><Icon className="w-4 h-4" /><span>{link.text}</span></Link>
+            )
+          })}
+        </NavWrapper>
+      </InnerSidebarContainer>
+
+      <InnerSidebarContainer>
+        <TagCloud />
+        <NavWrapper>
+          <Link href={`/settings`} className={`${pathname === '/settings' ? 'bg-primary-900' : null} text-sm rounded px-4 py-2 flex items-center space-x-2`}><Settings className="w-4 h-4" /><span>Settings</span></Link>
+          <button onClick={(e) => {
+            e.preventDefault()
+            logoutAction()
+          }} className="text-sm rounded px-4 py-2 flex items-center space-x-2"><LogOut className="w-4 h-4" /><span>Logout</span></button>
+        </NavWrapper>
+      </InnerSidebarContainer>
     </SidebarContainer>
   )
 }
--- packages/ui/src/components/Sidebar.tsx
+++ packages/ui/src/components/Sidebar.tsx
@@ -8,44 +8,60 @@ export type SidebarProps = {
   children: React.ReactNode
 }
 
-export const Sidebar = ({ siteTitle, memberCount, children }: SidebarProps) => {
+export const NavWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <nav className="flex flex-col space-y-2">
+      {children}
+    </nav>
+  )
+}
+
+export const SiteInfo = ({ siteTitle, memberCount }: { siteTitle: string, memberCount: number }) => {
+  return (
+    <div className="flex justify-between bg-primary-400 flex-col p-4 rounded space-y-2">
+      <h1 className="text-xl font-bold">{siteTitle}</h1>
+      <p className="flex items-center space-x-2 text-xs"><Globe className="w-4 h-4" /> <span>{memberCount} member{memberCount > 1 ? 's' : null}</span></p>
+    </div>
+  )
+}
+
+export const TagCloud = () => {
+  return (
+    <div className="flex flex-col px-4 space-y-2">
+      <h2 className="text-sm font-semibold">Tag cloud</h2>
+      <div className="flex flex-wrap gap-2">
+        <Badge>Next.js</Badge>
+        <Badge>React</Badge>
+      </div>
+    </div>
+  )
+}
+
+export const UserInfo = ({ username, points }: { username: string, points: number }) => {
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center space-x-3">
+        <h3 className="text-sm font-semibold">{username}</h3>
+        <p className="text-xs">{points} points</p>
+      </div>
+    </div>
+  )
+}
+
+
+export const InnerSidebarContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col space-y-4">
+      {children}
+    </div>
+  )
+}
+
+
+export const SidebarContainer = ({ children }: { children: React.ReactNode }) => {
   return (
     <aside className="flex justify-between flex-col py-4 px-4 w-64 border-r">
-      <div className="flex flex-col space-y-4">
-        <div className="flex justify-between bg-primary-400 flex-col p-4 rounded space-y-2">
-          <h1 className="text-xl font-bold">{siteTitle}</h1>
-          <p className="flex items-center space-x-2 text-xs"><Globe className="w-4 h-4" /> <span>{memberCount} member{memberCount > 1 ? 's' : null}</span></p>
-        </div>
-        <nav className="flex flex-col space-y-2">
-          {children}
-        </nav>
-      </div>
-
-      <div className="flex flex-col space-y-3">
-        <div className="flex flex-col">
-          <h2 className="text-sm font-semibold">Tag cloud</h2>
-          <div className="flex flex-wrap gap-2">
-            <Badge>Next.js</Badge>
-            <Badge>React</Badge>
-          </div>
-        </div>
-
-        <div className="flex flex-col">
-          <div className="flex items-center space-x-3 mb-4">
-            <div>
-              <h3 className="text-sm font-semibold">Henry Riventon</h3>
-              <p className="text-xs">100 points</p>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <p>Link</p>
-        </div>
-
-      </div>
-
-
+      {children}
     </aside>
   )
 }
