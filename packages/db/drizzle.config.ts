76b2822f921f73630430d5d095558dd4c006db0f
Dillon Raphael <dillon@creatorsneverdie.com>
2024-03-26 14:04:29 -0400

    fix drizzle config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -5,10 +5,6 @@ export default {
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    port: Number(process.env.DB_PORT)!,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME!,
+    connectionString: process.env.DATABASE_URL || ""
   }
 } satisfies Config;
